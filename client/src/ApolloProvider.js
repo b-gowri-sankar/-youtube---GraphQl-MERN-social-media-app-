afa49a35c94e1bede718548cd2aa85d7d79cfa7f
Gowri Sankar <sankaryadavrgukt@gmail.com>
2021-11-13 20:27:45 +0530

    added apollo provider

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,25 @@
+import React from "react";
+import App from "./App";
+
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	useQuery,
+	gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+	uri: "http://localhost:5000/",
+	cache: new InMemoryCache(),
+});
+
+const ApolloProviderInstance = () => {
+	return (
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	);
+};
+
+export default ApolloProviderInstance;
