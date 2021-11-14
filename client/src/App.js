a3a1d81b16794874ff1cb5261b87f989b2a60bbf
Gowri Sankar <sankaryadavrgukt@gmail.com>
2021-11-14 09:24:45 +0530

    setup menu bar and created routes and also  created dummy pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import { Container } from "semantic-ui-react";
+
 import "./App.css";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import MenuBar from "./components/MenuBar";
 
 const App = () => {
-	return <div>React app</div>;
+	return (
+		<Router>
+			<Container>
+				<MenuBar />
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route exact path="/login" element={<Login />} />
+					<Route exact path="/register" element={<Register />} />
+				</Routes>
+			</Container>
+		</Router>
+	);
 };
 
 export default App;
---
+++ client/src/components/MenuBar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+const MenuBar = () => {
+	const [activeItem, setActiveItem] = React.useState("home");
+
+	const handleItemClick = (e, { name }) => setActiveItem(name);
+
+	React.useEffect(() => {
+		const pathname = window.location.pathname;
+		const path = pathname === "/" ? "home" : pathname.substr(1);
+		setActiveItem(path);
+	}, []);
+
+	return (
+		<Menu pointing secondary size="massive">
+			<Menu.Item
+				name="home"
+				active={activeItem === "home"}
+				onClick={handleItemClick}
+				as={Link}
+				to="/"
+			/>
+			<Menu.Menu position="right">
+				<Menu.Item
+					name="login"
+					active={activeItem === "login"}
+					onClick={handleItemClick}
+					as={Link}
+					to="/Login"
+				/>
+				<Menu.Item
+					name="register"
+					active={activeItem === "register"}
+					onClick={handleItemClick}
+					as={Link}
+					to="/register"
+				/>
+			</Menu.Menu>
+		</Menu>
+	);
+};
+
+export default MenuBar;
